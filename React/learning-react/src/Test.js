430804e959be5fb068222f77553e0ab6630c793f
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-06 13:29:59 -0800

    update

--- React/learning-react/src/Test.js
+++ React/learning-react/src/Test.js
@@ -1,21 +1,37 @@
-const name = 'Khoa Hoang';
-const currenDate = new Date();
-const year = currenDate.getFullYear();
 
-const customStyle = {
-    color: "red",
+const currenDate = new Date(2024, 1, 1, 11);
+
+const currentTime = currenDate.getHours();
+
+
+
+
+const h1Style = {
+    color: "Red",
     fontSize: "20px",
-    border: "2px solid black"
 }
 
-const List = () => {
+const partOfDay = {
+    part: "Morning"
+}
+
+if (currentTime >= 12 && currentTime <= 18){
+    partOfDay.part = "Afternoon";
+    h1Style.color = "Green";
+} else if (currentTime > 18 && currentTime <= 24) {
+    partOfDay.part = "Evening";
+    h1Style.color = "Blue";
+}
+
+
+
+const Test = () => {
     return (
         <div>
-            <p style={customStyle}>Create by: {name}</p>
-            <p>Copyright {year}</p>
+            <h1 style={h1Style}>Good {partOfDay.part}</h1>
             
         </div>
     );
 }
 
-export default List
+export default Test
--- React/learning-react/src/index.js
+++ React/learning-react/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import List from './Test';
+// import App from './App';
+import Test from './Test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         
-        <List />
+        <Test />
     </React.StrictMode>
 );
 
