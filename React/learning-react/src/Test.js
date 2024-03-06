3c2ccd6dc910a45e98c06bdae968c771743838d6
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-05 16:30:20 -0800

    update

---
+++ React/learning-react/src/Test.js
@@ -0,0 +1,17 @@
+const List = () => {
+    return (
+        <div>
+            <h2>
+                Fav Food
+            </h2>
+
+            <ul>
+                <li>Bacon</li>
+                <li>Sandwich</li>
+                <li></li>
+            </ul>
+        </div>
+      );
+}
+ 
+export default List
