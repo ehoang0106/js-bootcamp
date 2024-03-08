3d5daf9f534176c2c4667c3ee761d115d1eef8c9
Khoa Hoang <khoahoang1698@gmail.com>
2024-03-08 12:26:21 -0800

    update

---
+++ React/emoji-meaning/src/emojipedia.js
@@ -0,0 +1,20 @@
+const emojipedia = [
+    {
+        id: 1,
+        emoji: "💪",
+        name: "Tense Biceps",
+        meaning:
+            "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+        id: 2,
+        emoji: "🙏",
+        name: "Person With Folded Hands",
+        meaning:
+            "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    }
+
+];
+
+
+export default emojipedia;
